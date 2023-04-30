bdfd8cf182a1b81974bef0835abad2dbc200de2d
dtruong46me <dnhtruong46.cs.pyth@gmail.com>
2023-04-30 21:20:31 +0700

    lab2

---
+++ lab2/AimsProject/Cart.java
@@ -0,0 +1,38 @@
+package lab2.AimsProject;
+
+public class Cart {
+    private static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered;
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered >= MAX_NUMBERS_ORDERED) {
+            System.out.println("The cart is almost full!");
+            return;
+        }
+        itemsOrdered[qtyOrdered++] = disc;
+        System.out.println("The disc has been added");
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].equals(disc)) {
+                for (int j = i; j < qtyOrdered - 1; j++) {
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                }
+                qtyOrdered--;
+                System.out.println("The disc has been removed");
+                return;
+            }
+        }
+        System.out.println("The disc was not found in the cart");
+    }
+
+    public float getTotalCost() {
+        float total = 0.0f;
+        for (int i = 0; i < qtyOrdered; i++) {
+            total += itemsOrdered[i].getCost();
+        }
+        return total;
+    }
+}
