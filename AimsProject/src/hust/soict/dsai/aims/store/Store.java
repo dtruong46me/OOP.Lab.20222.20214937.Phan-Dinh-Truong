8ca3682d6016598637f848bb0930aa4c3460cafc
dtruong46me <dnhtruong46.cs.pyth@gmail.com>
2023-06-06 08:54:34 +0700

    update readme

--- AimsProject/src/hust/soict/dsai/aims/store/Store.java
+++ AimsProject/src/hust/soict/dsai/aims/store/Store.java
@@ -30,19 +30,64 @@ public class Store {
     }
 
     public void showItems() {
-        System.out.println("\n\u001B[33m------------------ STORE ITEMS ------------------");
+        // System.out.println("\n\u001B[33m------------------ STORE ITEMS ------------------");
+        // if (itemsInStore.size() == 0) {
+        //     System.out.println("             <Your store is empty!>");
+        // }
+        // int i = 0;
+        // for (Media media : itemsInStore) {
+        //     System.out.printf("%d. %s\n",i+1, media.getDetail());
+        //     i += 1;
+        // }
+        // System.out.println("-------------------------------------------------\n\u001B[37m");
+
+        System.out.println("\n\u001B[33m+----------------------------------------------------------------+");
+        System.out.println("|                          STORE ITEMS                           |");
+        System.out.println("+----+--------+--------------------------+------------+----------+");
+        System.out.println("| No.|   ID   |         Title            |  Category  |   Cost   |");
+        System.out.println("+----+--------+--------------------------+------------+----------+");
         if (itemsInStore.size() == 0) {
-            System.out.println("             <Your store is empty!>");
+            System.out.println("|                    < Your store is empty!>                     |");
+            System.out.println("+----------------------------------------------------------------+");
+        } else {
+            int index = 0;
+            for (Media media : itemsInStore) {
+                int id = media.getId();
+                String title = media.getTitle();
+                if (title.length() > 24) {
+                    title = title.substring(0, 22) + "..";
+                }
+                String category = media.getCategory();
+                if (category.length() > 10) {
+                    category = category.substring(0, 8) + "..";
+                }
+                float cost = media.getCost();
+                index += 1;
+                System.out.printf("| %-2s | %-6s | %-24s | %-10s |  %5s$  |\n", index, id, title, category, cost);
+                // System.out.println("+----+--------+--------------------------+------------+----------+");
+
+
+            }
+            System.out.println("+----+--------+--------------------------+------------+----------+\u001B[37m");
         }
-        int i = 0;
-        for (Media media : itemsInStore) {
-            System.out.printf("%d. %s\n",i+1, media.getDetail());
-            i += 1;
-        }
-        System.out.println("-------------------------------------------------\n\u001B[37m");
     }
 
     public ArrayList<Media> getItemsInStore() {
         return itemsInStore;
     }
 }
+
+/*
+A123456789B123456789C123456789D123456789E123456789F123456789G12345
++----------------------------------------------------------------+
+|                          STORE ITEMS                           |
++----+--------+--------------------------+------------+----------+
+| No.|   ID   |         Title            |  Category  |   Cost   |
++----+--------+--------------------------+------------+----------+
+| 9  | 100009 | To Kill a Mocking Bird   |  Fiction   |  15.99$  |
++----+--------+--------------------------+------------+----------+
+| 10 | 100003 | Harry Potter and the P.. |  Fantasy   |  12.99$  |
++----+--------+--------------------------+------------+----------+
+| 6  | 100006 | Java: A Beginner's Guide | Programm.. |  19.99$  |
++----+--------+--------------------------+------------+----------+
+ */
--- AimsProject/src/hust/soict/dsai/test/store/StoreTest.java
+++ AimsProject/src/hust/soict/dsai/test/store/StoreTest.java
@@ -1,35 +1,94 @@
 package AimsProject.src.hust.soict.dsai.test.store;
 
-import AimsProject.src.hust.soict.dsai.aims.media.DigitalVideoDisc;
-import AimsProject.src.hust.soict.dsai.aims.store.Store;
+import java.util.ArrayList;
+
+import AimsProject.src.hust.soict.dsai.aims.media.Media;
 
 public class StoreTest {
-    public static void main(String[] args) {
-        Store store = new Store();
+    private ArrayList<Media> itemsInStore = new ArrayList<>();
 
-        store.showItems();
+    public void addMedia(Media media) {
+        itemsInStore.add(media);
+        System.out.println("\u001B[32mTHE MEDIA '" + media.getTitle() + "' HAS BEEN ADDED TO THE STORE!\u001B[37m");
+    }
 
-        // Add Media to the store
-        DigitalVideoDisc media1 = new DigitalVideoDisc("The Lion King", 
-            "Animation", 19.95f, "Roger Allers", 87);
-        DigitalVideoDisc media2 = new DigitalVideoDisc("Star Wars: A New Hope", 
-            "Science Fiction", 24.95f, "George Lucas", 124);
-        DigitalVideoDisc media3 = new DigitalVideoDisc("Harry Potter and the Philosopher's Stone", 
-            "Fantasy", 27.98f, "Chris Columbus", 152);
-        DigitalVideoDisc media4 = new DigitalVideoDisc("Aladin", 
-            "Animation", 18.99f);
-        DigitalVideoDisc media5 = new DigitalVideoDisc("Avatar 2", 
-            "Sci-fi/Action", 26.4f, "Jame Cameron", 190);
+    public void removeMedia(Media media) {
+        if (itemsInStore.remove(media)) {
+            System.out.println("\u001B[32mTHE MEDIA '" + media.getTitle() + "' HAS BEEN REMOVED FROM THE STORE!\u001B[37m");
+        } else {
+            System.out.println("\u001B[31mTHE MEDIA '" + media.getTitle() + "' IS NOT EXIST IN THE STORE!\u001B[37m");
+        }
+    }
 
-        store.addMedia(media1);
-        store.addMedia(media2);
-        store.addMedia(media3);
-        store.addMedia(media4);
-        store.addMedia(media5);
+    public Media search(String title) {
+        for (Media media : itemsInStore) {
+            if (media.getTitle().equalsIgnoreCase(title)) {
+                return media;
+            }
+        }
+        return null;
+    }
 
-        store.removeMedia(media2);
-        store.removeMedia(media2);
+    public void showItems() {
+        // System.out.println("\n\u001B[33m------------------ STORE ITEMS ------------------");
+        // if (itemsInStore.size() == 0) {
+        //     System.out.println("             <Your store is empty!>");
+        // }
+        // int i = 0;
+        // for (Media media : itemsInStore) {
+        //     System.out.printf("%d. %s\n",i+1, media.getDetail());
+        //     i += 1;
+        // }
+        // System.out.println("-------------------------------------------------\n\u001B[37m");
 
-        store.showItems();
+        System.out.println("\n\u001B[33m+----------------------------------------------------------------+");
+        System.out.println("|                          STORE ITEMS                           |");
+        System.out.println("+----+--------+--------------------------+------------+----------+");
+        System.out.println("| No.|   ID   |         Title            |  Category  |   Cost   |");
+        System.out.println("+----+--------+--------------------------+------------+----------+");
+        if (itemsInStore.size() == 0) {
+            System.out.println("|                    < Your store is empty!>                     |");
+            System.out.println("+----------------------------------------------------------------+");
+        } else {
+            int index = 0;
+            for (Media media : itemsInStore) {
+                int id = media.getId();
+                String title = media.getTitle();
+                if (title.length() > 24) {
+                    title = title.substring(0, 22) + "..";
+                }
+                String category = media.getCategory();
+                if (category.length() > 10) {
+                    category = category.substring(0, 8) + "..";
+                }
+                float cost = media.getCost();
+                index += 1;
+                System.out.printf("| %-2s | %-6s | %-24s | %-10s |  %5s$  |\n", index, id, title, category, cost);
+                // System.out.println("+----+--------+--------------------------+------------+----------+");
+
+
+            }
+            System.out.println("+----+--------+--------------------------+------------+----------+\u001B[37m");
+        }
+
+    }
+
+    public ArrayList<Media> getItemsInStore() {
+        return itemsInStore;
     }
 }
+
+/*
+A1234|6789B123|56789C123456789D123456789E|23456789F123|56789G12345
++----------------------------------------------------------------+
+|                          STORE ITEMS                           |
++----+--------+--------------------------+------------+----------+
+| No.|   ID   |         Title            |  Category  |   Cost   |
++----+--------+--------------------------+------------+----------+
+| 9  | 100009 | To Kill a Mocking Bird   |  Fiction   |  15.99$  |
++----+--------+--------------------------+------------+----------+
+| 10 | 100003 | Harry Potter and the P.. |  Fantasy   |  12.99$  |
++----+--------+--------------------------+------------+----------+
+| 6  | 100006 | Java: A Beginner's Guide | Programm.. |  19.99$  |
++----+--------+--------------------------+------------+----------+
+ */
