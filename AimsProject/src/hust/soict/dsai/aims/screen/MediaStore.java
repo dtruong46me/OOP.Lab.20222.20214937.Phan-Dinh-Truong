f5bef8f459560acb2f98fc26004c5bf7fa1d6a8c
dtruong46me <dnhtruong46.cs.pyth@gmail.com>
2023-06-20 19:27:54 +0700

    refactor screen to aims-project

---
+++ AimsProject/src/hust/soict/dsai/aims/screen/MediaStore.java
@@ -0,0 +1,46 @@
+package AimsProject.src.hust.soict.dsai.aims.screen;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import AimsProject.src.hust.soict.dsai.aims.media.Media;
+import AimsProject.src.hust.soict.dsai.aims.media.Playable;
+
+public class MediaStore extends JPanel{
+    public Media media;
+
+    public MediaStore(Media media) {
+        this.media = media;
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JLabel title = new JLabel(media.getTitle());
+        title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 20));
+        title.setAlignmentX(CENTER_ALIGNMENT);
+
+        JLabel cost = new JLabel("" + media.getCost() + "$");
+        cost.setAlignmentX(CENTER_ALIGNMENT);
+
+        JPanel container = new JPanel();
+        container.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        container.add(new JButton("Add to Cart"));
+        if (media instanceof Playable) {
+            container.add(new JButton("Play Media"));
+        }
+
+        this.add(Box.createVerticalGlue());
+        this.add(title);
+        this.add(cost);
+        this.add(Box.createVerticalGlue());
+        this.add(container);
+        this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
+}
