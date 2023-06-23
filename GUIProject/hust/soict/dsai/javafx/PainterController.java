d4e64989f347379e39d5bfb2ca7c1af58486851c
dtruong46me <dnhtruong46.cs.pyth@gmail.com>
2023-06-23 09:10:16 +0700

    finish painter

--- GUIProject/hust/soict/dsai/javafx/PainterController.java
+++ GUIProject/hust/soict/dsai/javafx/PainterController.java
@@ -4,13 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class PainterController {
-    // @FXML
-    // private Pane drawingAreaMouseDragged;
 
     @FXML
     private Pane drawingAreaPane;
@@ -22,9 +19,7 @@ public class PainterController {
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-        Circle newCircle = new Circle(event.getX(), event.getY(), 4);
-        newCircle.setFill(Color.BLACK);
+        Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
         drawingAreaPane.getChildren().add(newCircle);
     }
-
 }
